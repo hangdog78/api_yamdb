a16f7b295c98ffdf8ce67f908143aa26a26a193d
Andrey <nikonov23@gmail.com>
2022-06-01 00:38:41 +0300

    PP Day 2 v.2

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,44 @@
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256, requires=True)
+    slug = models.SlugField(unique=True, db_index=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256, requires=True)
+    slug = models.SlugField(unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256, requires=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    description =
+    category = 
+    genre = 
+    date = 
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Заголовок'
+        verbose_name_plural = 'Заголовки'
+
+    def __str__(self):
+        return self.name

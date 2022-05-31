4647def468725185f02038a1b29f6f8609d35e6c
hangdog <hangdog@mail.ru>
2022-05-31 10:34:03 +0400

    user model initial migration

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER = 1
+MODERATOR = 2
+ADMIN = 3
+SUPERUSER = 4
+
+ROLE_CHOICES = (
+          (USER, 'user'),
+          (MODERATOR, 'moderator'),
+          (ADMIN, 'admin'),
+          (SUPERUSER, 'superuser'),
+      )
+
+class User(AbstractUser):
+  role = models.PositiveSmallIntegerField(
+    choices=ROLE_CHOICES,
+    null=False
+    )
+  

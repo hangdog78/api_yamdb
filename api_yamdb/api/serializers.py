cba8825fc8f23542cc7641c818945900d3b29d78
hangdog <hangdog@mail.ru>
2022-05-31 17:20:49 +0400

    user api initial

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from users.models import User
 
@@ -8,3 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'email': {
+                'validators': [
+                    UniqueValidator(
+                        queryset=User.objects.all()
+                        )
+                    ]
+                },
+            'username': {
+                'validators': [
+                    UniqueValidator(
+                        queryset=User.objects.all()
+                        )
+                    ]
+                }
+            
+            }

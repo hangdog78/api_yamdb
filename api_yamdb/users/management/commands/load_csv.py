4f8affc1e658bbfd0ad7d538e96dd40f7fd0f582
hangdog <hangdog@mail.ru>
2022-06-01 16:14:08 +0400

    csv load command complete

--- api_yamdb/users/management/commands/load_csv.py
+++ api_yamdb/users/management/commands/load_csv.py
@@ -8,9 +8,15 @@ from django.conf import settings
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        CGR = '\033[92m'
+        CYELL = '\033[93m'
+        CEND = '\033[0m'
+
         data_dir = os.path.join(settings.BASE_DIR, 'static/data/')
+        print('=============== Loading csv files do database ===============')
         with open(data_dir + 'users.csv') as csvfile:
             reader = csv.DictReader(csvfile)
+            print('Loading' + CYELL + ' users.csv ' + CEND + 'file')
             for row in reader:
                 record = User(
                     id=row['id'],
@@ -22,4 +28,75 @@ class Command(BaseCommand):
                     last_name=row['last_name']
                 )
                 record.save()
-        print('csv files loading completed.')
+        
+        print('Loading' + CYELL + ' categoty.csv ' + CEND + 'file')
+        with open(data_dir + 'category.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = Category(
+                    id=row['id'],
+                    name=row['name'],
+                    slug=row['slug']
+                )
+                record.save()
+
+        print('Loading' + CYELL + ' genre.csv ' + CEND + 'file')
+        with open(data_dir + 'genre.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = Genre(
+                    id=row['id'],
+                    name=row['name'],
+                    slug=row['slug']
+                )
+                record.save()
+
+        print('Loading' + CYELL + ' titles.csv ' + CEND + 'file')
+        with open('titles.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = Title(
+                    id=row['id'],
+                    name=row['name'],
+                    year=row['year'],
+                    category=Category.objects.get(id=row['category'])
+                )
+                record.save()
+
+        print('Loading' + CYELL + ' genre_title.csv ' + CEND + 'file')
+        with open(data_dir + 'genre_title.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = GenreTitle(
+                    id=row['id'],
+                    title=Title.objects.get(id=row['title_id']),
+                    genre=Genre.objects.get(id=row['genre_id'])
+                )
+                record.save()
+
+        print('Loading' + CYELL + ' review.csv ' + CEND + 'file')
+        with open(data_dir + 'review.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = Review(
+                    id=row['id'],
+                    title=Title.objects.get(id=row['title_id']),
+                    text=row['text'], author=User.objects.get(id=row['author']),
+                    score=row['score'], pub_date=row['pub_date']
+                )
+                record.save()
+        
+        print('Loading' + CYELL + ' comments.csv ' + CEND + 'file')
+        with open(data_dir + 'comments.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                record = Comment(
+                    id=row['id'],
+                    review=Review.objects.get(id=row['review_id']),
+                    text=row['text'],
+                    author=User.objects.get(id=row['author']),
+                    pub_date=row['pub_date']
+                )
+                record.save()
+
+        print('===== Files loading' + CGR + ' complete.' + CEND + '=====')

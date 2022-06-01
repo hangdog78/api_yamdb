d0a5d973704a9aa934aad205689dae68f8feaa09
hangdog <hangdog@mail.ru>
2022-06-01 15:12:05 +0400

    csv users load command

---
+++ api_yamdb/users/management/commands/load_csv.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from users.models import User
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        data_dir = os.path.join(settings.BASE_DIR, 'static/data/')
+        with open(data_dir + 'users.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                p = User(
+                    id=row['id'],
+                    username=row['username'],
+                    email=row['email'],
+                    role=row['role'],
+                    bio=row['bio'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name']
+                )
+                p.save()
+        print('csv files loading completed.')

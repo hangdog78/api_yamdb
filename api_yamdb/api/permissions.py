5c7f5254b8c8cfc42a20f3fe385b5ad3d69d1c8e
hangdog <hangdog@mail.ru>
2022-06-01 11:03:52 +0400

    flake8 cleared

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class IsAdminPermission(permissions.BasePermission):
     '''Права доступа суперюзер или админ'''
     def has_permission(self, request, view):
-        return (request.user.is_authenticated and
-                request.user.is_admin)
+        return (request.user.is_authenticated
+                and request.user.is_admin)
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import (UserViewSet,
-                     signup_post,
-                     token_post)
+                    signup_post,
+                    token_post)
 
 api_v1_router = routers.DefaultRouter()
 api_v1_router.register('users', UserViewSet, basename='users')
